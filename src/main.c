f099e8c943aaa25c451be2d3e8d71a8cdb814e6d
AbdulSalam Ayeleru <93570850+Black-fox17@users.noreply.github.com>
2026-01-08 08:40:57 +0100

    main revampeed

--- src/main.c
+++ src/main.c
@@ -1,44 +1,36 @@
 #include "game.h"
 #include <stdio.h>
 
+symbolized_move_t read_move() {
+    symbolized_move_t sm = {0};
+    char buf[100];
+    if (fgets(buf, sizeof(buf), stdin)) {
+        sscanf(buf, " %c%c %c%c", &sm.from_row, &sm.from_col, &sm.to_row, &sm.to_col);
+    }
+    return sm;
+}
 int main(){
     game_t game;
-    char buffer[100];
 
     game_init(&game);
     printf("Peg Solitaire\n");
     printf("Enter move: FROM TO (e.g., D2 D4)\n");
     printf("Format: [RowLetter][ColNumber] [RowLetter][ColNumber]\n");
-    printf("Enter 'q' to quit.\n");
 
     game_display(&game);
 
-    while(1){
-        if(fgets(buffer, sizeof(buffer), stdin) != NULL){
-            if(buffer[0] == 'q'){
-                printf("Goodbye!\n");
-                break;
-            }else{
-                symbolized_move_t sym_move;
-                if(sscanf(buffer, "%c%c %c%c", &sym_move.from_row, &sym_move.from_col, &sym_move.to_row, &sym_move.to_col) == 4){
-                    if(game_play_move(&game, sym_move)){
-                        game_display(&game);
-                    } else {
-                        printf("Invalid move. Try again.\n");
-                    }
-                } else {
-                    printf("Invalid input format. Try again.\n");
-                }
-            }
+    while(!game_over(&game)){
+        symbolized_move_t sym_move = read_move();
+        if(game_play_move(&game, sym_move)){
+            game_display(&game);
+        } else {
+            printf("Invalid move. Try again.\n");
         }
-        if(game_over(&game)){
-            if (game_check_victory(&game)){
-                printf("Congratulations you just solved the puzzle!\n");
-            } else {
-                printf("Game Over! No more moves possible.\n");
-            }
-            break;
-        }
+    }
+    if (game_check_victory(&game)){
+        printf("Congratulations you just solved the puzzle!\n");
+    } else {
+        printf("Game Over! No more moves possible.\n");
     }
     return 0;
 }
